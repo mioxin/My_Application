3d03ec9a19664f3217c827f993bbac508dfcabac
MrMioxin <Mioxin@ya.ru>
2014-11-25 17:44:39 +0600

    2х строчный ListView

--- app/src/main/java/com/example/mmm/myapplication/about.java
+++ app/src/main/java/com/example/mmm/myapplication/about.java
@@ -12,25 +12,41 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SimpleExpandableListAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class about extends Activity {
+    String[] animals = new String[]{"Котики", "Собачки"};
     String[] catnames = new String[] { "Рыжик", "Барсик", "Мурзик",
-            "Мурка", "Васька", "Томасина", "Бобик", "Кристина", "Пушок",
-            "Дымка", "Кузя", "Китти", "Барбос", "Масяня", "Симба" };
-    String[] subscr = new String[] {"Самый интересный ","из трех инструментов, ","пожалуй",
+            "Мурка", "Васька", "Томасина", "Бобик"};
+    String[] dognames = new String[]{"Кристина", "Пушок",
+            "Дымка", "Кузя", "Китти",
+            "Барбос", "Масяня", "Симба" };
+    String[] subscr1 = new String[] {"Самый интересный ","из трех инструментов, ","пожалуй",
             "SAS Enterprise Miner, ","позволяющий строить применимые в ","бизнесе прогнозные модели, ",
-            "с  помощью ","построения диаграмм для анализа и многое другое.",
+            "с  помощью "};
+    String[] subscr2 = new String[] {"построения диаграмм для анализа и многое другое.",
             "Честно, это очень интересно и круто, ","но понятно, не многим.  ","Например, ",
             "студенты научатся: решать задачи поиска шаблонов ","(сегментация, ассоциация и анализ  последовательности)",
             " и прогнозного моделирования (деревья решений, линейная регрессия, логистическая  регрессия, ",
             "нейронные сети, ансамбль моделей, ассоциативные правила и другие  модели)"
     };
     int[] colors = new int[2];
+    ExpandableListView lView;
+    ArrayList<Map<String,String>> arAnimals;
+    ArrayList<Map<String,String>> arChildItems;
+    ArrayList<Map<String,String>> arChildSubscr;
+    ArrayList<ArrayList<Map<String,String>>> arChildData;
+    Map <String,String> m;
 
     //private ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,catnames);
 //    AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
@@ -45,30 +61,61 @@ public class about extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
 
-        ListView lView = (ListView)findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,catnames);
+        lView = (ExpandableListView)findViewById(R.id.eListView);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,catnames);
+//        lView.setAdapter(adapter);
+        // заполняем коллекцию групп из массива с названиями групп
+        arAnimals = new ArrayList<Map<String, String>>();
+        for (String group : animals) {
+// заполняем список аттрибутов для каждой группы
+            m = new HashMap<String, String>();
+            m.put("groupName", group);
+            arAnimals.add(m);
+        }
+// список аттрибутов групп для чтения
+        String groupFrom[] = new String[] {"groupName"};
+// список ID view-элементов, в которые будет помещены аттрибуты групп
+        int groupTo[] = new int[] {android.R.id.text1};
+// создаем коллекцию для коллекций элементов
+        arChildData = new ArrayList<ArrayList<Map<String, String>>>();
+        // создаем коллекцию элементов для первой группы
+        arChildItems = new ArrayList<Map<String, String>>();
+// заполняем список аттрибутов для каждого элемента
+        for (int i = 0; i < catnames.length; i++){
+            m = new HashMap<String, String>();
+            m.put("Name", catnames[i]);
+            m.put("Subscr", subscr1[i]);
+            arChildItems.add(m);
+        }
+// добавляем в коллекцию коллекций
+        arChildData.add(arChildItems);
+// создаем коллекцию элементов для второй группы
+        arChildItems = new ArrayList<Map<String, String>>();
+        for (int i = 0; i < dognames.length; i++){
+            //for (String cat : catnames) {
+            m = new HashMap<String, String>();
+            m.put("Name", dognames[i]);
+            m.put("Subscr", subscr2[i]);
+            arChildItems.add(m);
+        }
+        arChildData.add(arChildItems);
+        // список аттрибутов элементов для чтения
+        String childFrom[] = new String[] {"Name","Subscr"};
+// список ID view-элементов, в которые будет помещены аттрибуты элементов
+        int childTo[] = new int[] {R.id.tvItem, R.id.tvSmall};
+        SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+                this,
+                arAnimals,
+                android.R.layout.simple_expandable_list_item_1,
+                groupFrom,
+                groupTo,
+                arChildData,
+                R.layout.item,
+                childFrom,
+                childTo
+        );
         lView.setAdapter(adapter);
 
-        //setListAdapter(adapter);
-        colors[0] = Color.parseColor("#559966CC");
-        colors[1] = Color.parseColor("#55336699");
-
-//        LinearLayout lLayout = (LinearLayout) findViewById(R.id.lLayout);
-//        LayoutInflater ltInflater = getLayoutInflater();
-//        for (int i = 0; i < catnames.length; i++) {
-//            Log.d("myLogs", "i = " + i);
-//            View item = ltInflater.inflate(R.layout.item, lLayout, false);
-//            TextView tvItem = (TextView) item.findViewById(R.id.tvItem);
-//            tvItem.setText(catnames[i]);
-//            TextView tvSmall = (TextView) item.findViewById(R.id.tvSmall);
-//            tvSmall.setText("Описание: " + subscr[i]);
-//            ImageView iView = (ImageView) item.findViewById(R.id.iView);
-//            iView.setImageResource(R.drawable.kitty);
-//
-//            item.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
-//            item.setBackgroundColor(colors[i % 2]);
-//            lLayout.addView(item);
-//        }
     }
 
 //    @Override
